5c88e8657f00b68a064cc4250f5d7f81c47b9905
Orion <asiskrich@gmail.com>
2026-02-11 18:30:19 +0300

    transefer commit from vuz

--- backend/pyChatService/channels/base.py
+++ backend/pyChatService/channels/base.py
@@ -1,18 +1,21 @@
+from uuid import UUID
 from faststream.rabbit import RabbitRouter
 from faststream.annotations import Logger
 from faststream.rabbit.annotations import RabbitMessage
 
-from config.messaging import user_exchange, auth_exchange, session_queue
-from models.user import User
-from repos.user import UserRepo
+from config.messaging import chat_exchange,form_queue,user_queue,form_exchange,user_exchange
+from models import Chat,Message
+from services.message import MessageService
+from services.chat import ChatService
+from schemas.v1.chat import CreateChat
 
 router = RabbitRouter()
 
 
-@router.publisher(exchange=user_exchange, routing_key="user.info.public")
+
 @router.subscriber(
-    queue=session_queue,
-    exchange=auth_exchange,
+    queue=form_queue,
+    exchange=form_exchange,
 )
 async def handle_user_info(
     event: dict,
@@ -22,14 +25,25 @@ async def handle_user_info(
     logger.info(f"Received event {event}")
 
     try:
-        email = event.get("email")
-        sid = event.get("sid")
-        if not email:
-            raise ValueError("No email in event")
+        uid1 = str(event.get("like_sender"))
+        uid2 = str(event.get("form_creator"))
+        # TODO
+        # form matched -> both chatlists updated
+        # if user is blocked, ignore likes on notification service
+        # also if user is blocked mark chat as frozen and when its frozen no1 can type into it besides system
+        # notification service builds notifications in redis, when they're done it sends them
+        # listen to block user event to freeze
+        # how to notify about likes? do i show profile? how do i construct them?
+        # 
+        if not all([uid1,uid2]):
+            raise
 
-        user = await User.find_one(User.email == email)
+     
 
-        if user:
+        ce=CreateChat(owner_id=UUID(int=0),owner_type='system',type='private',status='active',allowed_users=[UUID(uid1),UUID(uid2)])
+        chat=await ChatService().create_chat(ce)
+
+        if chat:
             user_data = user.model_dump()
             user_data["sid"] = sid
             await msg.ack()
--- backend/pyChatService/config/messaging.py
+++ backend/pyChatService/config/messaging.py
@@ -2,19 +2,30 @@
 from faststream.rabbit import RabbitExchange, RabbitQueue, ExchangeType
 
 
+chat_exchange = RabbitExchange(
+    name="chat.events.topic",
+    type=ExchangeType.TOPIC,
+    durable=True,
+)
+
+form_exchange = RabbitExchange(
+    name="form.events.topic",
+    type=ExchangeType.TOPIC,
+    durable=True,
+)
+
 user_exchange = RabbitExchange(
     name="user.events.topic",
     type=ExchangeType.TOPIC,
     durable=True,
 )
 
-auth_exchange = RabbitExchange(
-    name="auth.events.topic",
-    type=ExchangeType.TOPIC,
-    durable=True,
+
+user_queue = RabbitQueue(
+    name="chat.user.queue", durable=True, routing_key="user.created.*"
 )
 
-
-session_queue = RabbitQueue(
-    name="user.session.queue", durable=True, routing_key="auth.session.*"
+form_queue = RabbitQueue(
+    name="chat.form.queue", durable=True, routing_key="form.matched.*"
 )
+
