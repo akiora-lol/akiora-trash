2b5190dece4b9db076dcd3c36157e247dffb7239
Orion <asiskrich@gmail.com>
2026-02-12 12:10:28 +0300

    channels init

--- backend/pyChatService/channels/base.py
+++ backend/pyChatService/channels/base.py
@@ -3,14 +3,19 @@ from faststream.rabbit import RabbitRouter
 from faststream.annotations import Logger
 from faststream.rabbit.annotations import RabbitMessage
 
-from config.messaging import chat_exchange,form_queue,user_queue,form_exchange,user_exchange
-from models import Chat,Message
+from config.messaging import (
+    chat_exchange,
+    form_queue,
+    user_queue,
+    form_exchange,
+    user_exchange,
+)
+from models import Chat, Message
 from services.message import MessageService
 from services.chat import ChatService
 from schemas.v1.chat import CreateChat
 
 router = RabbitRouter()
-
 
 
 @router.subscriber(
@@ -27,33 +32,18 @@ async def handle_user_info(
     try:
         uid1 = str(event.get("like_sender"))
         uid2 = str(event.get("form_creator"))
-        # TODO
-        # form matched -> both chatlists updated
-        # if user is blocked, ignore likes on notification service
-        # also if user is blocked mark chat as frozen and when its frozen no1 can type into it besides system
-        # notification service builds notifications in redis, when they're done it sends them
-        # listen to block user event to freeze
-        # how to notify about likes? do i show profile? how do i construct them?
-        # 
-        if not all([uid1,uid2]):
+
+        if not all([uid1, uid2]):
             raise
 
-     
-
-        ce=CreateChat(owner_id=UUID(int=0),owner_type='system',type='private',status='active',allowed_users=[UUID(uid1),UUID(uid2)])
-        chat=await ChatService().create_chat(ce)
-
-        if chat:
-            user_data = user.model_dump()
-            user_data["sid"] = sid
-            await msg.ack()
-            return user_data
-        else:
-            user = await UserRepo.create_user(email)
-            user_data = user.model_dump()
-            user_data["sid"] = sid
-            await msg.ack()
-            return user_data
+        ce = CreateChat(
+            owner_id=UUID(int=0),
+            owner_type="system",
+            type="private",
+            status="active",
+            allowed_users=[UUID(uid1), UUID(uid2)],
+        )
+        chat = await ChatService().create_chat(ce)
 
     except Exception as e:
         logger.error(f"Error: {str(e)}")
