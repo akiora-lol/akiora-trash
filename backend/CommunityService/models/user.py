d15c5d04c788ce66bbb853efac0e4c8de2857e41
Orion <asiskrich@gmail.com>
2026-02-19 21:16:33 +0300

    refactor: community service naming

--- backend/CommunityService/models/user.py
+++ backend/CommunityService/models/user.py
@@ -1,5 +1,5 @@
 from beanie import Document
-from pydantic import ConfigDict, Field, EmailStr, field_serializer
+from pydantic import ConfigDict, Field, EmailStr, field_serializer, BaseModel
 from uuid import UUID, uuid4
 from typing import Literal
 from datetime import datetime, UTC
@@ -17,32 +17,33 @@ def default_roles():
     return ["default"]
 
 
+SocialKey = Literal["vk", "tg", "ds", "yt", "tw", "sc"]
+
+
+class Social(BaseModel):
+    type: Literal["personal", "public"]
+    link: str
+    hidden: False
+
+
 class User(Document):
     id: UUID = Field(default_factory=uuid4)
     email: EmailStr
     nickname: str = Field(default_factory=default_name)
     gender: Literal["male", "female"] | None = None
     age: int | None = Field(default=None, min=15)
-    socials: dict | None = None
+    socials: dict[SocialKey, list[Social]] = Field(default_factory=dict)
     roles: list[str] = Field(default_factory=default_roles)
     created_at: datetime = Field(default_factory=time_now)
     last_updated: datetime = Field(default_factory=time_now)
 
-    # @field_serializer("id")
-    # def serialize_id(self, id: UUID):
-    #     return str(id)
+    @field_serializer("created_at")
+    def serialize_ca(self, dt: datetime):
+        return dt.isoformat()
 
-    # @field_serializer("created_at")
-    # def serialize_ca(self, dt: datetime):
-    #     return dt.isoformat()
-
-    # @field_serializer("last_activity")
-    # def serialize_la(self, dt: datetime):
-    #     return dt.isoformat()
-
-    # @field_serializer("exipres_at")
-    # def serialize_ea(self, dt: datetime):
-    #     return dt.isoformat()
+    @field_serializer("last_updated")
+    def serialize_la(self, dt: datetime):
+        return dt.isoformat()
 
     model_config = ConfigDict(extra="ignore")
 
---
+++ backend/CommunityService/routes/broker/main.py
@@ -0,0 +1,24 @@
+from faststream.redis import RedisRouter, RedisMessage, Redis, StreamSub
+from faststream import Logger
+from dishka import FromDishka
+
+from services import AuthService
+from settings import Settings
+
+
+settings = Settings()
+
+router = RedisRouter()
+
+
+@router.subscriber(stream=StreamSub("user-rpc", maxlen=100))
+async def validate_sid(
+    msg: str | dict,
+    logger: Logger,
+    auth_service: FromDishka[AuthService],
+):
+    logger.info(f"incoming msg: {msg}")
+    data = await auth_service.verify_session_handler(msg)
+    logger.info(f"returm msg: {data}")
+
+    return data
